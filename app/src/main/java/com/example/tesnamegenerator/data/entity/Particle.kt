fda7be6addc790afa2f47af5a8057f9b5852a077
MatteoL <matteolorenz99@gmail.com>
2024-12-10 17:02:33 +0100

    created entity representing name particle

---
+++ app/src/main/java/com/example/tesnamegenerator/data/entity/Particle.kt
@@ -0,0 +1,10 @@
+package com.example.tesnamegenerator.data.entity
+
+data class Particle(
+    val value: String,
+    val source: String,
+    val race: String,
+    val gender: String?,
+    val type: String
+)
+
